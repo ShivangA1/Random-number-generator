21f63347a104aaf8753dc915e1f88ccd84e403f0
Shivang Jaiswal <shivangjaiswal77@gmail.com>
2026-07-23 15:53:33 +0530

    Add files via upload

---
+++ RandomNum/main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("submit");
+const num = document.getElementById("mainNum")
+
+btn.onclick = randomise;
+
+function randomise() {
+    const floatcheck = document.getElementById("float").checked;
+    const min = Number(document.getElementById("min").value);
+    const max = Number(document.getElementById("max").value);
+    if (max <= min) {
+        alert("invalid parameters");
+        return
+    }
+    var rnd = (Math.random() * ((max + 1) - min)) + min;
+    if (floatcheck) {
+        num.textContent = rnd.toFixed(2);
+        console.log(rnd);
+    }
+    else {
+        num.textContent = Math.floor(rnd);
+    }
+}
